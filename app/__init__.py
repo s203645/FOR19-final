820e23ae39e508e7552ab2b06a0bae2197d09afb
Sara Birkeland <s203645@nhhs.no>
2023-04-27 17:15:16 +0200

    design kalkulator

--- app/__init__.py
+++ app/__init__.py
@@ -15,12 +15,12 @@ application.config['SECRET_KEY'] = "80e49e4bea0c03d64cc40d37f11535b85e93880b43c8
 #application.config['SQLALCHEMY_DATABASE_URI'] = DBVAR 
 #application.config['SQLALCHEMY_BINDS'] ={'transport': DBVAR}
 
-application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@127.0.0.1/for19' 
+#application.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@127.0.0.1/for19'
 
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-application.config['SQLALCHEMY_POOL_SIZE'] = 50
-application.config['SQLALCHEMY_MAX_OVERFLOW'] = 50
-application.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False # No debug intercepts
+#application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+#application.config['SQLALCHEMY_POOL_SIZE'] = 50
+#application.config['SQLALCHEMY_MAX_OVERFLOW'] = 50
+#application.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False # No debug intercepts
 ### Code computer
 # application.config['SECRET_KEY'] = '3oueqkfdfas8ruewqndr8ewrewrouewrere44554'
 # DBVAR = 'sqlite:///user.db'
