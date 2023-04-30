57bbed40224cbbc1a1fceb00359f2298645e27a3
Sara Birkeland <s203645@nhhs.no>
2023-04-30 14:01:56 +0200

    design charts

--- app/carbon_app/routes.py
+++ app/carbon_app/routes.py
@@ -9,7 +9,6 @@ from sqlalchemy import cast, Date, func
 carbon_app=Blueprint('carbon_app',__name__)
 
 efco2={
-
         'Bus':{'Diesel':0.10231,'CNG':0.08,'Petrol':0.10231,'No Fossil Fuel':0},
         'Car':{'Petrol':0.18592,'Diesel':0.16453,'No Fossil Fuel':0, 'Electric':0},
         'Bus':{'Diesel':0.10231,'CNG':0.08,'Petrol':0.10231,'No Fossil Fuel':0, 'Electric':0},
@@ -17,21 +16,20 @@ efco2={
         'Plane':{'Petrol':0.24298},
         'Ferry':{'Diesel':0.11131, 'CNG':0.1131, 'No Fossil Fuel':0},
         'Motorbike':{'Petrol':0.09816,'No Fossil Fuel':0},
-        'Scooter':{'No Fossil Fuel':0},
         'Bicycle':{'No Fossil Fuel':0},
-        'Walk':{'No Fossil Fuel':0}
+        'Walk':{'No Fossil Fuel':0},
+        'Train':{'Diesel':1.1e-4,'Electric':0}
     }
 
 efch4={
-        'Bus':{'Diesel':2e-5,'CNG':2.5e-3,'Petrol':2e-5,'No Fossil Fuel':0},
         'Bus':{'Diesel':2e-5,'CNG':2.5e-3,'Petrol':2e-5,'No Fossil Fuel':0, 'Electric':0},
         'Car':{'Petrol':3.1e-4,'Diesel':3e-6,'No Fossil Fuel':0, 'Electric':0},
         'Plane':{'Petrol':1.1e-4},
         'Ferry':{'Diesel':3e-5, 'CNG':3e-5,'No Fossil Fuel':0},
         'Motorbike':{'Petrol':2.1e-3,'No Fossil Fuel':0},
-        'Scooter':{'No Fossil Fuel':0},
         'Bicycle':{'No Fossil Fuel':0},
-        'Walk':{'No Fossil Fuel':0}
+        'Walk':{'No Fossil Fuel':0},
+        'Train':{'Diesel':1.1e-4,'Electric':0}
     }
 
 transport_dict = {
